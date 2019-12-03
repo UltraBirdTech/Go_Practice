1859acf813a62c389a902203d976643e87f29e73
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-12-04 06:41:05 +0900

    add fibonacci

---
+++ 02_fibonacci/fibonacci.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main(){
+    var first int
+    first = 0
+    var second int
+    second = 1
+    var result int
+    for i := 0; result < 1000; i++ {
+        result = first + second
+        fmt.Print(result)
+        fmt.Printf("\n")
+        first = second
+        second = result
+    }
+
+}
